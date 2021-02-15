5107b915213b79a50bfd3abf152256d2e02a0b76
Lai, Chun-Hao <54859651+ja841014@users.noreply.github.com>
2021-02-15 10:36:43 +0800

    delete the parentMap. refactor code

--- CS561HW1/src/SearchAlgorithm.java
+++ CS561HW1/src/SearchAlgorithm.java
@@ -4,14 +4,19 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.channels.FileChannel.MapMode;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.Stack;
+import java.util.TreeMap;
+import java.util.TreeSet;
 //always be given a valid input.txt file 
 //When the wagon moves vertically or horizontally (so north, south, east, or west), the movement cost is will be either 1 for BFS or 10 for UCS and A. 
 //When the wagon moves diagonally (so northeast, northwest, southeast, or southwest), the movement cost is either 1 for BFS or 14 for UCS and A. 
@@ -21,7 +26,7 @@ public class SearchAlgorithm {
 	private static int COLS;
 	private static int ROWS;
 	public static void main(String[] args) throws IOException {
-		File file = new File("/Users/laicunhao/eclipse-workspace/CS561HW1/src/test2.txt");
+		File file = new File("/Users/laicunhao/eclipse-workspace/CS561HW1/src/test1.txt");
 		File outFile = new File("output.txt");
 		FileWriter fileWriter = new FileWriter(outFile);
 		Scanner scanner = new Scanner(file);
@@ -36,20 +41,19 @@ public class SearchAlgorithm {
 		int numEndGoal = Integer.parseInt(scanner.next());
 		
 		int [][] MAP = new int[ROWS][COLS];
-		int [][][] parentMap = new int[ROWS][COLS][2];
 		List<String> endLocs = new ArrayList<>();
 		
 		storeEndLoc(numEndGoal, scanner, endLocs);
 		buildMap(ROWS, COLS, MAP, scanner);
-		System.out.println(algorithm.equals("BFS"));
+		
 		if(algorithm.equals("BFS")) {
-			BFS(MAP, parentMap, endLocs, start, maxRockHeight, fileWriter);
+			BFS(MAP, endLocs, start, maxRockHeight, fileWriter);
 		}
 		else if(algorithm.equals("UCS")) {
-			UCS(MAP, parentMap, endLocs, start, maxRockHeight,fileWriter);
+			UCS(MAP, endLocs, start, maxRockHeight,fileWriter);
 		}
 		else if(algorithm.equals("A*")) {
-			AstartSearch(MAP, parentMap, endLocs, start, maxRockHeight, fileWriter);
+			AstartSearch(MAP, endLocs, start, maxRockHeight, fileWriter);
 		}
 		
 		int a = 10;
@@ -60,29 +64,40 @@ public class SearchAlgorithm {
 	/*
 	 * A star search
 	 * */
-	private static void AstartSearch(int[][] MAP, int[][][] parentMap, List<String> endLocs, int[] startPtr, int maxRockHeight, FileWriter fileWriter) throws IOException {
-		PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> a.cost - b.cost);
+	private static void AstartSearch(int[][] MAP, List<String> endLocs, int[] startPtr, int maxRockHeight, FileWriter fileWriter) throws IOException {
+		
+		
+		Map<String, Node> endGoalEntry = new HashMap<>();
 		
 		for(int goal = 0; goal < endLocs.size(); goal++) {
 			
+			PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> a.cost - b.cost);
+			Map<String, Node> inQueue = new HashMap<>();
+			
 			Set<String> visited = new HashSet<>();
+			
 			String[] tmpEndPoint = endLocs.get(goal).split(",");
 			int[] endPoint = new int[] {Integer.parseInt(tmpEndPoint[0]), Integer.parseInt(tmpEndPoint[1])};
-			Node startNode = new Node(startPtr, 0 + findStraightLineDistance(startPtr, endPoint), new int[] {-1,-1} );
-			pq.offer(startNode);
+			Node startNode = new Node(startPtr, 0 + findStraightLineDistance(startPtr, endPoint), null );
 			
+			pq.offer(startNode);
+			inQueue.put(startPtr[0] + "," + startPtr[1], null);
 			// this statement may wrong
-			while(!pq.isEmpty() ) {
+			while(!pq.isEmpty() && !visited.contains(endLocs.get(goal)) ) {
 
 				Node curNode = pq.poll();
+				inQueue.remove(curNode.loc[0] + "," + curNode.loc[1]);
+				
 				// only when we poll out we mark it visited
 				String visitNode = curNode.loc[0] + "," + curNode.loc[1];
 				if(visited.contains(visitNode)) {
 					continue;
 				}
-
+				if(endLocs.contains(visitNode)) {
+					endGoalEntry.put(visitNode, curNode);
+				}
 				// put the parent loc into it
-				parentMap[curNode.loc[0]][curNode.loc[1]] = curNode.parent;
+//				parentMap[curNode.loc[0]][curNode.loc[1]] = curNode.parent;
 				
 				visited.add(visitNode);
 				
@@ -94,21 +109,33 @@ public class SearchAlgorithm {
 						Node nextNode;
 						// if there is x or y == 0, it means they should be  one direction in N E W S
 						if(dir[0] == 0 || dir[1] == 0) {
-							nextNode = new Node(new int[] {x, y}, 10 + heightDifferenceAndMud(MAP, x, y, curNode.loc) + curNode.cost, new int[] {curNode.loc[0], curNode.loc[1]});
+							nextNode = new Node(new int[] {x, y}, 10 + heightDifferenceAndMud(MAP, x, y, curNode.loc) + curNode.cost, curNode );
 						}
 						else {
-							nextNode = new Node(new int[] {x, y}, 14 + heightDifferenceAndMud(MAP, x, y, curNode.loc) + curNode.cost, new int[] {curNode.loc[0], curNode.loc[1]});
+							nextNode = new Node(new int[] {x, y}, 14 + heightDifferenceAndMud(MAP, x, y, curNode.loc) + curNode.cost, curNode );
 						}
+						
+						String checkNode = nextNode.loc[0] + "," + nextNode.loc[1];
+						if( inQueue.containsKey(checkNode) ) {
+							if(inQueue.get(checkNode).cost > nextNode.cost) {
+								pq.remove(inQueue.get(checkNode));
+							}
+							else {
+								continue;
+							}						
+						}	
 						pq.offer(nextNode);
-		
+						inQueue.put(checkNode, nextNode);	
+						
 					}
 				}
 			}
+			// check whether we reach the end goal
 			if(!visited.contains(endLocs.get(goal))) {
 				fileWriter.write("FAIL");
 			}
 			else {
-				writeOutPath(fileWriter, parentMap, endLocs.get(goal), startPtr);
+				writeOutPath(fileWriter, endGoalEntry.get(endLocs.get(goal)), startPtr);
 			}
 			if(goal != endLocs.size() - 1) {
 				fileWriter.write("\n");
@@ -119,6 +146,191 @@ public class SearchAlgorithm {
 		
 		
 	}
+	
+	/**
+	 * UCS Algorithm
+	 * */
+	private static void UCS(int[][] MAP, List<String> endLocs, int[] startPtr, int maxRockHeight, FileWriter fileWriter) throws IOException {
+		PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> a.cost - b.cost);
+		Map<String, Node> inQueue = new HashMap<>();
+		
+		Map<String, Node> endGoalEntry = new HashMap<>();
+		
+		Set<String> visited = new HashSet<>();
+		int cntRemainGoal = endLocs.size();
+		
+		pq.offer(new Node(startPtr, 0, null));
+		inQueue.put(startPtr[0] + "," + startPtr[1], null);
+		// this statement may wrong // !pq.isEmpty()
+		while( !pq.isEmpty() && cntRemainGoal != 0) {
+			if(cntRemainGoal == 0) {
+				break;
+			}
+
+			Node curNode = pq.poll();
+			inQueue.remove(curNode.loc[0] + "," + curNode.loc[1]);
+			// only when we poll out we mark it visited
+			String visitNode = curNode.loc[0] + "," + curNode.loc[1];
+			if(visited.contains(visitNode)) {
+				continue;
+			}
+			if(endLocs.contains(visitNode)) {
+				endGoalEntry.put(visitNode, curNode);
+				cntRemainGoal--;
+			}
+			// put the parent loc into it
+//			parentMap[curNode.loc[0]][curNode.loc[1]] = curNode.parent;
+			
+			visited.add(visitNode);
+			
+			for(int[] dir : DIRECTION) {
+				int x = curNode.loc[0] + dir[0];
+				int y = curNode.loc[1] + dir[1];
+				
+				
+				if(isValidCell(x, y) && isValidClimb(MAP, x, y, curNode.loc, maxRockHeight) && !visited.contains(x + "," + y) ) {
+					
+					Node nextNode;
+					// if there is x or y == 0, it means they should be  one direction in N E W S
+					if(dir[0] == 0 || dir[1] == 0) {
+						nextNode = new Node(new int[] {x, y}, 10 + curNode.cost, curNode);
+					}
+					else {
+						nextNode = new Node(new int[] {x, y}, 14 + curNode.cost, curNode);
+					}
+					
+					String checkNode = nextNode.loc[0] + "," + nextNode.loc[1];
+					if( inQueue.containsKey(checkNode) ) {
+						if(inQueue.get(checkNode).cost > nextNode.cost) {
+							pq.remove(inQueue.get(checkNode));
+						}
+						else {
+							continue;
+						}						
+					}
+					
+					pq.offer(nextNode);
+					inQueue.put(checkNode, nextNode);
+				}
+			}
+		}
+		
+		for(int i = 0; i < endLocs.size(); i++) {
+			if(!visited.contains(endLocs.get(i))) {
+				fileWriter.write("FAIL");
+			}
+			else {
+				writeOutPath(fileWriter, endGoalEntry.get(endLocs.get(i)), startPtr);
+			}
+			// Not sure what is the chnge line character \n \r
+			if(i != endLocs.size() - 1) {
+				fileWriter.write("\n");
+			}
+		}
+		fileWriter.close();
+		
+	}
+	
+	
+	/**
+	 * BFS Algorithm not finish the path Map bug
+	 * */
+	private static void BFS(int[][] MAP, List<String> endLocs, int[] startPtr, int maxRockHeight, FileWriter fileWriter) throws IOException {
+		Queue<Node> queue = new LinkedList<>();
+		Set<String> visited = new HashSet<>();
+		
+		Map<String, Node> endGoalEntry = new HashMap<>();
+		
+		int cntRemainGoal = endLocs.size();
+		Node start = new Node(startPtr, 0, null);
+		queue.offer(start);
+		visited.add(startPtr[0] + "," + startPtr[1]);
+		// this statement may wrong
+		while(!queue.isEmpty() && cntRemainGoal != 0) {
+			// all targets are already reached
+			if(cntRemainGoal == 0) {
+				break;
+			}
+			int size = queue.size();
+			for(int i = 0; i < size; i++) {
+				Node currNode = queue.poll();
+				
+				for(int[] dir : DIRECTION) {
+					int x = currNode.loc[0] + dir[0];
+					int y = currNode.loc[1] + dir[1];
+					// the cell not exceed the board and can climb and not visited
+					if(isValidCell(x, y) && isValidClimb(MAP, x, y, currNode.loc, maxRockHeight) && !visited.contains(x + "," + y)) {
+						// x, y is new node, we put parent node info(tmp) into parentMap
+//						parentMap[x][y] = currNode;
+						
+						Node nextNode = new Node(new int[]{x, y}, 0, currNode);
+						queue.offer(nextNode);
+						
+						String nextStr = x + "," + y;
+						visited.add(nextStr);
+						if(endLocs.contains(nextStr)) {
+							endGoalEntry.put(nextStr, nextNode);
+							cntRemainGoal--;
+						}
+						
+					}
+				}
+				
+			}
+		}
+		
+		for(int i = 0; i < endLocs.size(); i++) {
+			if(!visited.contains(endLocs.get(i))) {
+				fileWriter.write("FAIL");
+			}
+			else {
+				writeOutPath(fileWriter, endGoalEntry.get(endLocs.get(i)), startPtr);
+			}
+			// Not sure what is the chnge line character \n \r
+			if(i != endLocs.size() - 1) {
+				fileWriter.write("\n");
+			}
+		}
+		fileWriter.close();
+	}
+	/**
+	 * This function is used to write the Path into file by using stack
+	 * */
+	private static void writeOutPath(FileWriter fileWriter, Node endLoc, int[] startPtr) throws IOException {
+		// TODO Auto-generated method stub
+		Stack<String> stack = new Stack<>();
+//		String[] loc = endLoc.split(",");
+		int x = endLoc.loc[0];
+		int y = endLoc.loc[1];
+		// need to change because the question is suck
+		stack.add(y + "," + x);
+//		while(x != startPtr[0] || y != startPtr[1]) {
+//			int[] parent = parentMap[x][y];
+//			x = parent[0];
+//			y = parent[1];
+//			// need to change because the question is suck
+//			stack.add(y + "," + x);
+//		}
+		
+		for(Node node = endLoc;  node.parent != null; node = node.parent) {
+			x = node.parent.loc[0];
+			y = node.parent.loc[1];
+			stack.add(y + "," + x);
+		}
+		// write on the file
+		int size = stack.size();
+		for(int i = 0; i < size; i++) {
+			String tmpString = stack.pop();
+			fileWriter.write(tmpString);
+			// if it is last point we break out.  we do not want add a extra whitespace
+			if(i == size - 1) {
+				break;
+			}
+			fileWriter.write(" ");
+		}
+		
+	}
+	
 	/*
 	 * Only used for A* search.
 	 * We calculate height difference + Mud level and return it.
@@ -140,156 +352,6 @@ public class SearchAlgorithm {
 		}
 		// return height difference
 		return Math.abs(newNodeHeight - oldNodeHeight) + newNodeMud;
-	}
-	
-	/**
-	 * UCS Algorithm
-	 * */
-	private static void UCS(int[][] MAP, int[][][] parentMap, List<String> endLocs, int[] startPtr, int maxRockHeight, FileWriter fileWriter) throws IOException {
-		PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> a.cost - b.cost);
-		Set<String> visited = new HashSet<>();
-		int cntRemainGoal = endLocs.size();
-		pq.offer(new Node(startPtr, 0, new int[] {-1,-1}));
-		// this statement may wrong
-		while(!pq.isEmpty() || cntRemainGoal == 0) {
-			if(cntRemainGoal == 0) {
-				break;
-			}
-
-			Node curNode = pq.poll();
-			// only when we poll out we mark it visited
-			String visitNode = curNode.loc[0] + "," + curNode.loc[1];
-			if(visited.contains(visitNode)) {
-				continue;
-			}
-			if(endLocs.contains(visitNode)) {
-				cntRemainGoal--;
-			}
-			// put the parent loc into it
-			parentMap[curNode.loc[0]][curNode.loc[1]] = curNode.parent;
-			
-			visited.add(visitNode);
-			
-			for(int[] dir : DIRECTION) {
-				int x = curNode.loc[0] + dir[0];
-				int y = curNode.loc[1] + dir[1];
-				if(isValidCell(x, y) && isValidClimb(MAP, x, y, curNode.loc, maxRockHeight) && !visited.contains(x + "," + y)) {
-					
-					Node nextNode;
-					// if there is x or y == 0, it means they should be  one direction in N E W S
-					if(dir[0] == 0 || dir[1] == 0) {
-						nextNode = new Node(new int[] {x, y}, 10 + curNode.cost, new int[] {curNode.loc[0], curNode.loc[1]});
-					}
-					else {
-						nextNode = new Node(new int[] {x, y}, 14 + curNode.cost, new int[] {curNode.loc[0], curNode.loc[1]});
-					}
-					pq.offer(nextNode);
-	
-				}
-			}
-		}
-		for(int i = 0; i < endLocs.size(); i++) {
-			if(!visited.contains(endLocs.get(i))) {
-				fileWriter.write("FAIL");
-			}
-			else {
-				writeOutPath(fileWriter, parentMap, endLocs.get(i), startPtr);
-			}
-			// Not sure what is the chnge line character \n \r
-			if(i != endLocs.size() - 1) {
-				fileWriter.write("\n");
-			}
-		}
-		fileWriter.close();
-		
-	}
-	
-	
-	/**
-	 * BFS Algorithm
-	 * */
-	private static void BFS(int[][] MAP, int[][][] parentMap, List<String> endLocs, int[] startPtr, int maxRockHeight, FileWriter fileWriter) throws IOException {
-		Queue<int[]> queue = new LinkedList<>();
-		Set<String> visited = new HashSet<>();
-		int cntRemainGoal = endLocs.size();
-		queue.offer(startPtr);
-		visited.add(startPtr[0] + "," + startPtr[1]);
-		// this statement may wrong
-		while(!queue.isEmpty() || cntRemainGoal == 0) {
-			// all targets are already reached
-			if(cntRemainGoal == 0) {
-				break;
-			}
-			int size = queue.size();
-			for(int i = 0; i < size; i++) {
-				int[] currNode = queue.poll();
-				
-				for(int[] dir : DIRECTION) {
-					int x = currNode[0] + dir[0];
-					int y = currNode[1] + dir[1];
-					// the cell not exceed the board and can climb and not visited
-					if(isValidCell(x, y) && isValidClimb(MAP, x, y, currNode, maxRockHeight) && !visited.contains(x + "," + y)) {
-						// x, y is new node, we put parent node info(tmp) into parentMap
-						parentMap[x][y] = currNode;
-						queue.offer(new int[]{x, y});
-						
-						String nextStr = x + "," + y;
-						visited.add(nextStr);
-						if(endLocs.contains(nextStr)) {
-							cntRemainGoal--;
-						}
-						
-					}
-				}
-				
-			}
-		}
-		
-		for(int i = 0; i < endLocs.size(); i++) {
-			if(!visited.contains(endLocs.get(i))) {
-				fileWriter.write("FAIL");
-			}
-			else {
-				writeOutPath(fileWriter, parentMap, endLocs.get(i), startPtr);
-			}
-			// Not sure what is the chnge line character \n \r
-			if(i != endLocs.size() - 1) {
-				fileWriter.write("\n");
-			}
-		}
-		fileWriter.close();
-	}
-	/**
-	 * This function is used to write the Path into file by using stack
-	 * */
-	private static void writeOutPath(FileWriter fileWriter, int[][][] parentMap, String endLoc, int[] startPtr) throws IOException {
-		// TODO Auto-generated method stub
-		Stack<String> stack = new Stack<>();
-		String[] loc = endLoc.split(",");
-		int x = Integer.parseInt(loc[0]);
-		int y = Integer.parseInt(loc[1]);
-		// need to change because the question is suck
-		stack.add(y + "," + x);
-		while(x != startPtr[0] || y != startPtr[1]) {
-			int[] parent = parentMap[x][y];
-			x = parent[0];
-			y = parent[1];
-			// need to change because the question is suck
-			stack.add(y + "," + x);
-		}
-		// write on the file
-		int size = stack.size();
-		for(int i = 0; i < size; i++) {
-			String tmpString = stack.pop();
-			System.out.print(tmpString);
-			fileWriter.write(tmpString);
-			// if it is last point we break out.  we do not want add a extra whitespace
-			if(i == size - 1) {
-				break;
-			}
-			fileWriter.write(" ");
-		}
-		
 	}
 	
 	/**
@@ -355,9 +417,9 @@ public class SearchAlgorithm {
 	 * */
 	public static class Node {
 		int[] loc;
-		int[] parent;
+		Node parent;
 		int cost;
-		public Node(int[] loc, int cost, int[] parent) {
+		public Node(int[] loc, int cost, Node parent) {
 			this.loc = loc;
 			this.cost = cost;
 			this.parent = parent;
